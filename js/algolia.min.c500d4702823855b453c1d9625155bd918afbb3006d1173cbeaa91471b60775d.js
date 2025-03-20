31d586f530b83ff06e0bad9430e5279ca9bbbee1
feng0530 <fff80705@gmail.com>
2025-03-20 15:23:41 +0800

    update with website files

---
+++ js/algolia.min.c500d4702823855b453c1d9625155bd918afbb3006d1173cbeaa91471b60775d.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function(){const n=function(){const e=instantsearch({indexName:"algolia",searchClient:algoliasearch("Y68VT16ZWI","fe4fd21b7ac9972a70f8e470ca1d3208")});e.addWidgets([instantsearch.widgets.searchBox({container:"#searchbox",placeholder:"搜尋內容..."}),instantsearch.widgets.hits({container:"#hits",templates:{item:e=>{console.log(e);let n=e.permalink,s=e._highlightResult.title.matchedWords.join(","),t="";return e.tags.forEach((e)=>{t+=`<p class="tag">#${e}</p>`}),`
+                            <a href="${n}" class="algolia-hit-item-link js-algolia-search-result" data-search-keyword="${s}">
+                                ${e._highlightResult.title.value}
+                                <p>${e.description}</p>
+                                ${t}
+                            </a>
+                        `},empty:e=>`
+                            <div class="hit-item">
+                                <h3>找不到相關內容....</h3>
+                            </div>
+                        `,cssClasses:{item:"algolia-hit-item"}}}),instantsearch.widgets.configure({hitsPerPage:5}),instantsearch.widgets.pagination({container:"#pagination"})]),e.start()},e=document.getElementById("searchModal"),s=document.getElementById("openModalBtn"),o=document.getElementById("closeModalBtn");let t=!1;s.addEventListener("click",()=>{e.style.display="block",t||(n(),t=!0)}),o.addEventListener("click",()=>{e.style.display="none"}),window.addEventListener("click",t=>{t.target===e&&(e.style.display="none")})})
